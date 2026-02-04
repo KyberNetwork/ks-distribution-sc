96e56d673044afd6067a46e995d7e7e0329fc71f
Vu Tran <vutt94@gmail.com>
2026-02-04 10:15:05 +0700

    chore: update updateRoot script for handle multiple chains, multiple campaigns

--- script/Base.s.sol
+++ script/Base.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import 'ks-common-sc/script/Base.s.sol';
+import 'ks-common-sc-latest/script/Base.s.sol';
 import 'openzeppelin-contracts/contracts/utils/Address.sol';
 
 contract BaseDistributorScript is BaseScript {
@@ -13,6 +13,23 @@ contract BaseDistributorScript is BaseScript {
     bytes funcSelector;
     string name;
     bool status;
+  }
+
+  mapping(uint256 => address) adminOf;
+  mapping(uint256 => address) distributorOf;
+  mapping(uint256 => address[]) guardiansOf;
+  mapping(uint256 => address[]) operatorsOf;
+  mapping(uint256 => address[]) rescuersOf;
+
+  function _loadConfigs(string[] memory _chainIds) internal override {
+    for (uint256 i = 0; i < _chainIds.length; i++) {
+      uint256 chainId = vm.parseUint(_chainIds[i]);
+      adminOf[chainId] = _readAddressByChainId('admin', chainId);
+      distributorOf[chainId] = _readAddressByChainId('distributor', chainId);
+      guardiansOf[chainId] = _readAddressArrayByChainId('guardians', chainId);
+      operatorsOf[chainId] = _readAddressArrayByChainId('operators', chainId);
+      rescuersOf[chainId] = _readAddressArrayByChainId('rescuers', chainId);
+    }
   }
 
   function _readClaimingAmounts(string memory path, uint256 idx)
@@ -50,7 +67,7 @@ contract BaseDistributorScript is BaseScript {
     )
   {
     string memory json = _getJsonString(key);
-    bytes memory data = json.parseRaw(string.concat('.', chainId));
+    bytes memory data = json.parseRaw(string.concat('.', vm.toString(vm.getChainId())));
     Hook[] memory hooks = abi.decode(data, (Hook[]));
 
     addresses = new address[](hooks.length);
--- script/UpdateRoot.s.sol
+++ script/UpdateRoot.s.sol
@@ -4,34 +4,74 @@ pragma solidity ^0.8.0;
 import '../src/KSDistributor.sol';
 import './Base.s.sol';
 
-contract UpdateRootScript is BaseScript {
+contract UpdateRootScript is BaseDistributorScript {
   using stdJson for string;
 
-  bytes32 campaignId = bytes32(0);
-  uint256 effectiveTimestamp = type(uint256).max;
+  KSDistributor distributor;
 
-  function run() external {
-    require(campaignId != bytes32(0), 'campaignId must be set to a non-zero value');
+  // @dev Update root for multiple campaigns on multiple chains
+
+  // Usage:
+  // # Update root for multiple campaigns using chainIds, campaignIds, and effectiveTimestamps
+  // forge script UpdateRootScript \
+  // --sig "run(string[],bytes32[],uint256[])" \
+  // "[1,8453,8453]" \
+  // "[0x4620df13f805b0f4948a11733c325a2befb35115bb121a359f5ecdaa111bfc4c,0x6b0c58c3c5d752b7ebba28c104e012c932fcc21683efa3b1559b4c76f1d2e0a3,0x9385788dc841a66c2845a4b1384c116519aef01e25433e7930ee5aec2edac6ba]" \
+  // "[1770204199,1770204199,1770204199]" \
+  //  --broadcast
+
+  function run(
+    string[] memory chainIds,
+    bytes32[] memory campaignIds,
+    uint256[] memory effectiveTimestamps
+  ) public multiChain(chainIds) {
     require(
-      effectiveTimestamp != type(uint256).max, 'effectiveTimestamp must be set to a valid value'
+      chainIds.length == campaignIds.length && campaignIds.length == effectiveTimestamps.length,
+      'length mismatch'
     );
 
-    uint256 chainId;
-    assembly {
-      chainId := chainid()
+    distributor = KSDistributor(payable(distributorOf[vm.getChainId()]));
+
+    for (uint256 i = 0; i < campaignIds.length; i++) {
+      bytes32 campaignId = campaignIds[i];
+      (uint256 startTimestamp,,) = IKSDistributor(distributor).campaigns(campaignId);
+
+      //this means the campaign exists on this chain, start updating
+      if (startTimestamp != 0) {
+        string memory filePath =
+          string.concat('script/output/campaign-', vm.toString(campaignId), '.json');
+        string memory jsonString = vm.readFile(filePath);
+        bytes32 root = jsonString.readBytes32('.root');
+
+        require(root != bytes32(0), 'root is empty');
+
+        _updateRoot(campaignId, root, effectiveTimestamps[i]);
+        _verifyRoot(campaignId, root, effectiveTimestamps[i]);
+      }
     }
+  }
 
-    string memory filePath =
-      string.concat('script/output/campaign-', vm.toString(campaignId), '.json');
-    string memory jsonString = vm.readFile(filePath);
-    bytes32 root = jsonString.readBytes32('.root');
-
-    address payable distributor = payable(_readAddress('distributor'));
-
-    vm.startBroadcast();
-    console.log('Campaign:', vm.toString(campaignId));
+  function _updateRoot(bytes32 campaignId, bytes32 root, uint256 effectiveTimestamp) internal {
+    console.log(
+      'Updating campaignId: %s on chainId: %s',
+      vm.toString(campaignId),
+      vm.toString(vm.getChainId())
+    );
     console.log('Root:', vm.toString(root));
+    console.log('effectiveTimestamp:', effectiveTimestamp);
+
     KSDistributor(distributor).submitRoot(campaignId, root, effectiveTimestamp);
-    vm.stopBroadcast();
+
+    console.log('Root updated successfully');
+  }
+
+  function _verifyRoot(bytes32 campaignId, bytes32 root, uint256 effectiveTimestamp) internal {
+    (bytes32 newRoot, uint256 newEffectiveTimestamp) =
+      IKSDistributor(distributor).pendingRoots(campaignId);
+
+    require(newRoot == root, 'root mismatch');
+    require(newEffectiveTimestamp == effectiveTimestamp, 'effectiveTimestamp mismatch');
+
+    console.log('Root verified successfully');
   }
 }
